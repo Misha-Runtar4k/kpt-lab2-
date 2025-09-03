1128674496962b03bcc42487ceeb39b84335ac37
Misha-Runtar4k <misabuc81@gmail.com>
2025-09-03 23:32:18 +0300

    Add project
    added the first half of the lab otk

---
+++ KPT lab 2 Misha Buts/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KPT_lab_2_Misha_Buts
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            long b;
+            byte c;
+            Console.WriteLine("Enter a:");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter b:");
+            b = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine("Enter c:");
+            c = Convert.ToByte(Console.ReadLine());
+            Console.WriteLine("Result : " + (a - b + c));
+            Console.WriteLine("Result : " + (a + b + c));
+            Console.WriteLine("Result : " + (a - (b - c)));
+                }
+    }
+}
