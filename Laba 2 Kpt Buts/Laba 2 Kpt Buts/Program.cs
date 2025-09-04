0d68f5f20f8817d49f96e26ddd2736d143322122
Misha-Runtar4k <misabuc81@gmail.com>
2025-09-04 19:01:29 +0300

    add project
    second part of laboratory work 2

---
+++ Laba 2 Kpt Buts/Laba 2 Kpt Buts/Program.cs
@@ -0,0 +1,15 @@
+﻿double a;
+double b;
+Console.WriteLine("Enter a:");
+a = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Enter b:");
+b = Convert.ToDouble(Console.ReadLine());
+
+
+double test = a * b * b / 2;
+Console.Write("test Resolt: ");
+Console.WriteLine(test);
+
+
+Console.WriteLine("Result : " + (a * b * b / 2).ToString("F4"));
+Console.WriteLine("Result : " + (Math.Round(a / (b - a), 2)));
